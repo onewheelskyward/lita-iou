657bb72f242d3452e55dd978d5ac3a96a5600272
Andrew Kreps <andrew.kreps@gmail.com>
2015-04-21 23:58:44 -0700

    First shot at !iouing beer!

---
+++ README.md
@@ -0,0 +1,25 @@
+# lita-iou
+
+[![Build Status](https://travis-ci.org/onewheelskyward/lita-iou.png?branch=master)](https://travis-ci.org/onewheelskyward/lita-iou)
+[![Coverage Status](https://coveralls.io/repos/onewheelskyward/lita-iou/badge.png)](https://coveralls.io/r/onewheelskyward/lita-iou)
+
+TODO: Bot tracking of IOUs.
+
+## Installation
+
+Add lita-iou to your Lita instance's Gemfile:
+
+``` ruby
+gem "lita-iou"
+```
+
+## Configuration
+
+Redis Req'd
+
+## Usage
+
+!iou buddy
+!ious | !iou status | !iou stats
+!iou clear buddy
+
---
+++ lib/lita/handlers/iou.rb
@@ -0,0 +1,29 @@
+module Lita
+  module Handlers
+    class Iou < Handler
+      REDIS_KEY = 'iou'
+      route(/^!iou\s+([^\s]+)$/i, :add_iou)
+
+      def add_iou(response)
+        ower = response.user.name
+        owee = response.matches[0][0]
+        key = REDIS_KEY + ".#{ower}"
+        iou_count = redis.hget(key, owee).to_i
+        iou_count += 1
+        redis.hset(key, owee, iou_count)
+        response.reply "#{ower} now owes #{owee} #{iou_count} #{beer_icons iou_count.to_i }."
+      end
+
+      def beer_icons(count)
+        str = ''
+        0..count.times do
+          str += "ð"
+        end
+        str
+      end
+
+    end
+
+    Lita.register_handler(Iou)
+  end
+end
